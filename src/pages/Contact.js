e15d5a46ee7a3497727b21e8e5a52c9ec6e5daba
bjarkt <bjarke.tobiesen@gmail.com>
2019-05-01 22:51:27 +0200

    add contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,6 +1,16 @@
 import React from "react";
 export const Contact = () => (
-  <div>
-    <p>Contact</p>
-  </div>
+  <section>
+    <h2>Contact</h2>
+    <ul>
+      <li>
+        <p>
+          Email:{" "}
+          <a href="mailto:bjarke.tobiesen@gmail.com">
+            bjarke.tobiesen@gmail.com
+          </a>
+        </p>
+      </li>
+    </ul>
+  </section>
 );
