10904814c8bcd680ecdf33ca850c2136c98c7f3a
bjarkt <bjarke.tobiesen@gmail.com>
2021-10-28 19:52:44 +0200

    version api

--- deployment.sh
+++ deployment.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-docker-compose build && docker-compose push
+docker-compose build $@ && docker-compose push
 docker stack deploy --compose-file=docker-compose.yml --with-registry-auth personal-website
